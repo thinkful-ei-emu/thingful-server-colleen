517d7785c00454d3faf715fac6ae7edbed0e4c6f
Colleen <colleenhiggins87@gmail.com>
2019-08-02 13:21:08 -0700

    databas url

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@ module.exports = {
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
   NODE_ENV: process.env.NODE_ENV || 'development',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
-  DB_URL: process.env.DB_URL || 'postgresql://thingful@localhost/thingful',
+  DB_URL: process.env.DATABASE_URL || 'postgresql://thingful@localhost/thingful',
 }
